68b5c4132df928d5912a4e97681065bd924f151f
Matt Kenney <mkenney@mjk-tech.com>
2024-09-30 19:44:37 -0400

    correct create-invalidation paths

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ npm run build
 . ./aws.local
 cd dist
 aws s3 cp --recursive . "s3://${BUCKET}"
-aws cloudfront create-invalidation --distribution-id "${DISTRIBUTION_ID}" --paths "/ /index.html /manifest.json"
+aws cloudfront create-invalidation --distribution-id "${DISTRIBUTION_ID}" --paths / /index.html /manifest.json
